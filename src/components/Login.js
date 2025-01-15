d7a8485b8f895196ce14b50a182a1a5fb3f1c190
Rahul Adupa <68019226+rahuladupa@users.noreply.github.com>
2025-01-15 15:17:53 -0600

    Reapply "Implemented login singup and sigout feature integated firbase 🔥"
    This reverts commit 3da27294e45601447cfaad996c32cb2664b572a0.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,132 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidationData } from "../utils/validate";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateEmail, updateProfile } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errMessage, setErrorMessage] = useState(null);
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const toggleSignUpForm = () => {
     setIsSignInForm(!isSignInForm);
+  };
+  const handelButtonclick = () => {
+    const message = checkValidationData(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(message);
+    if (message) return;
+    if (!isSignInForm) {
+
+
+      // sign up logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/68019226?v=4"
+          }).then(() => {
+            const { uid, email, displayName, photoURL } = auth.currentUser
+            dispatch(addUser({
+              uid : uid,
+              email:email,
+              displayName:displayName,
+              photoURL:photoURL
+            })
+              
+            )
+            navigate("/browse")
+          }).catch((error) => {
+           setErrorMessage(error.message)
+          });
+        })
+        .catch((error) => {
+          const errCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errCode , errorMessage)
+        });
+    } else {
+      // sign in logic
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user)
+        navigate("/browse")
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+        setErrorMessage(errorCode, errorMessage)
+        navigate("/")
+      })
+
+    }
   };
   return (
     <div>
       <Header />
       <div className="absolute  bg-opacity-80 ">
-        <img className=" brightness-50"
+        <img
+          className=" brightness-50"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/US-en-20250106-TRIFECTA-perspective_65e335d4-6f1e-4d03-8daa-e439fbaaa340_large.jpg"
           alt="netflix_banner"
         />
       </div>
-      <form className=" rounded-md absolute bg-black w-3/12 p-12 my-36 mx-auto right-0 left-0 text-white bg-opacity-80 ">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className=" rounded-md absolute bg-black w-3/12 p-12 my-36 mx-auto right-0 left-0 text-white bg-opacity-70 "
+      >
         <h1 className="text-3xl font-bold py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
-        {!isSignInForm &&  <input
-          type="text"
-          placeholder="Full Name"
-          className=" rounded-lg w-full p-4 mx-1 my-3 bg-black bg-opacity-30 border border-white-500"
-        />}
+        {!isSignInForm && (
+          <input
+          ref={name}
+            type="text"
+            placeholder="Full Name"
+            className=" rounded-lg w-full p-4 mx-1 my-3 bg-black bg-opacity-30 border border-white-500"
+          />
+        )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className=" rounded-lg w-full p-4 mx-1 my-3 bg-black bg-opacity-30 border border-white-500"
         />
-       
+
         <input
+          ref={password}
           type="password"
           placeholder="Enter password"
           className=" rounded-lg w-full p-4 mx-1 my-3 bg-black bg-opacity-30 border border-white-500"
         />
-        <button className="bg-red-600 w-full p-4 mx-1 my-3 rounded-lg">
-        {isSignInForm ? "Sign In" : "Sign Up"}        </button>
+        <p className="text-red-500 font-bold text-lg">{errMessage}</p>
+        <button
+          onClick={handelButtonclick}
+          className="bg-red-600 w-full p-4 mx-1 my-3 rounded-lg"
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}{" "}
+        </button>
         <p onClick={toggleSignUpForm} className="py-4 hover:cursor-pointer">
-        {isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered?Sign up now"}
-          </p>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered?Sign up now"}
+        </p>
       </form>
     </div>
   );
